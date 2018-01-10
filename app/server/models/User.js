febf32f8f55200e1532f36549bdf0b16977ae8af
Ben Weinshel <ben@weinshel.net>
2018-01-09 21:24:40 -0600

    one more place to add years

--- app/server/models/User.js
+++ app/server/models/User.js
@@ -362,7 +362,7 @@ schema.statics.validateProfile = function(profile, cb){
     profile.name.length > 0 &&
     profile.adult &&
     profile.school.length > 0 &&
-    ['2018', '2019', '2020', '2021'].indexOf(profile.graduationYear) > -1 &&
+    ['2018', '2019', '2020', '2021', '2022later', 'other'].indexOf(profile.graduationYear) > -1 &&
     ['M', 'F', 'O', 'N'].indexOf(profile.gender) > -1 &&
     profile.pronouns.length > 0
     ));
